aaf851f6a956f961873c800ad062865330d4c660
Reid Eastin <s198012@students.aie.edu>
2019-08-21 09:14:58 -0500

    Add project files.

---
+++ SimpleRpg/Game.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleRpg
+{
+    class Game
+    {
+        public void Start()
+        {
+            Console.WriteLine("Welcome!");
+            Console.ReadKey();
+        }
+       
+    }
+}
