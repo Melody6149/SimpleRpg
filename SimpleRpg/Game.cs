298fb12cf0c4eb6476eba145c796e833787f0da7
Reid Eastin <s198012@students.aie.edu>
2019-08-21 10:46:17 -0500

    added in easy way to set how many monsters show up also made it easy to set damage and added in way for game to know if you are alive

--- SimpleRpg/Game.cs
+++ SimpleRpg/Game.cs
@@ -8,18 +8,40 @@ namespace SimpleRpg
 {
     class Game
     {
+        String playerName = ""; // allows for player name to be stored for later
+        int PlayerHealth = 100; // for player healt
+
         public void Start()
         {
-            String playerName = ""; //
-            int PlayerHealth = 100; // for player healt
+            Welcome(); // runs code in welcome
+            bool alive = true; // bool allows for it to be set true or false
+            int monsterRemaining = 5; //alows you to set number of monsters that show up
+
+            while (alive && monsterRemaining > 0) // fight until you die
+            {
+                Console.WriteLine("There are " + monsterRemaining + " monsters remaining.");
+                alive = Encounter(5); // sets damage
+                monsterRemaining--; // subtracts one from monster remaing
+            }
+           
+
+
+
+
+            Console.ReadKey(); // waits for key to move on to next line
+        }
+        void Welcome() // code that is run when welcom () is run
+        {
             Console.WriteLine("What is Your name?"); // shows what is your name
             playerName = Console.ReadLine(); // allows player to put in there name and have it stored using the string typed earlier
             Console.WriteLine("Welcome " + playerName + "."); // shows playername because i put in the
-            
+        }
 
+        bool Encounter(int monsterDamage) // Code for encounter is here so it can be started with Encounter in the () in the start you can changed monster damage
+        {
             //Monster encounter!
-            int monsterDamage = 13; // Sets monster's damage
-            Console.WriteLine("");
+            
+            Console.WriteLine(""); // creates a space inbetween lines
             Console.WriteLine("There is a monser in front of you"); // displayes this text
 
             string action = ""; //allows response to be stored as action
@@ -32,17 +54,22 @@ namespace SimpleRpg
                 Console.WriteLine("the monster attacks! " + playerName + "takes " + monsterDamage + " damage!");
                 PlayerHealth = PlayerHealth - monsterDamage;
                 Console.WriteLine(playerName + " has " + PlayerHealth + " health remaining.");
+                if (PlayerHealth<= 0 ) //runs code if player health is bellow 0
+                {
+                    Console.WriteLine(playerName + " was defeated");
+                    return false; // Return leaves monster encounter code and sets alive to false
+                }
                 //Player attack
-                Console.WriteLine(playerName + " attacks! The monster is defeated!");
+                Console.WriteLine(playerName + " attacks! The monster is defeated!"); // only shows if the code in if is not run
             }
             else if (action == "flee") // does this if player types in flee
             {
                 //Esccape
                 Console.WriteLine("Got away safely..."); // shows got away safely
             }
-
-            Console.ReadKey(); // waits for key to move on to next line
+            return true;
         }
+        
        
     }
 }
