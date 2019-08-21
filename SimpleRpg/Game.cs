bb74617c85d116c7e00256616ad61d10587aa6a3
Reid Eastin <s198012@students.aie.edu>
2019-08-21 09:51:23 -0500

    added new code there is a choice it now accepts playername shows text based on what you pick now left lots of  comments for later in the code

--- SimpleRpg/Game.cs
+++ SimpleRpg/Game.cs
@@ -10,8 +10,38 @@ namespace SimpleRpg
     {
         public void Start()
         {
-            Console.WriteLine("Welcome!");
-            Console.ReadKey();
+            String playerName = ""; //
+            int PlayerHealth = 100; // for player healt
+            Console.WriteLine("What is Your name?"); // shows what is your name
+            playerName = Console.ReadLine(); // allows player to put in there name and have it stored using the string typed earlier
+            Console.WriteLine("Welcome " + playerName + "."); // shows playername because i put in the
+            
+
+            //Monster encounter!
+            int monsterDamage = 13; // Sets monster's damage
+            Console.WriteLine("");
+            Console.WriteLine("There is a monser in front of you"); // displayes this text
+
+            string action = ""; //allows response to be stored as action
+            Console.WriteLine("What will you do? (fight/flee)"); //displayes this text
+            action = Console.ReadLine(); // stores user input as the string typed earlier
+
+            if (action == "fight") // does this if player types in fight
+            {
+                //Monster attack
+                Console.WriteLine("the monster attacks! " + playerName + "takes " + monsterDamage + " damage!");
+                PlayerHealth = PlayerHealth - monsterDamage;
+                Console.WriteLine(playerName + " has " + PlayerHealth + " health remaining.");
+                //Player attack
+                Console.WriteLine(playerName + " attacks! The monster is defeated!");
+            }
+            else if (action == "flee") // does this if player types in flee
+            {
+                //Esccape
+                Console.WriteLine("Got away safely..."); // shows got away safely
+            }
+
+            Console.ReadKey(); // waits for key to move on to next line
         }
        
     }
